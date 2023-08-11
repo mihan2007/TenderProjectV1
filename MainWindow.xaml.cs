d01a0ee97b85ac0debd65994b2d7de3e02c46c33
mihan2007 <52691119+mihan2007@users.noreply.github.com>
2023-08-11 10:40:53 +0300

    create button

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace TenderProject
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
