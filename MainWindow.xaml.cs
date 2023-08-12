2c0f1619497d050b69e7f87a79eb6fda97d5b48a
mihan2007 <52691119+mihan2007@users.noreply.github.com>
2023-08-12 22:20:43 +0300

    create passport and list of tenders

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,11 +23,26 @@ namespace TenderProject
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            TenderList.ItemsSource = new[] { new { Subject = "бинты", Customer = "Мэрия Мухсранска", ExpirationDate = "10/09/2023", Law ="44ФЗ", Link = "https:/zakupki"},
+            new { Subject = "гондоны", Customer = "Мэрия УстьПиздюйск", ExpirationDate = "11/09/2023", Law ="223ФЗ", Link = "https:/zakupki"}};
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Close();
+            PassportTender passportTender = new PassportTender();
+            passportTender.Show();
+
+        }
+
+        private void TenderList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            PassportTender passportTender = new PassportTender();
+            passportTender.Show();
         }
     }
 }
