7fc92bc06661c794975ae9ecfd264ca346116694
mihan2007 <52691119+mihan2007@users.noreply.github.com>
2023-08-20 22:03:38 +0300

    add scrowiever? min size of the window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.IO;
-
 
 namespace TenderProject
 {
+
     public partial class MainWindow : Window
     {
+        public const string DirectoryPath = @"C:\tenderproject\";
+        public const string Extension = "txt";
+        
         public class TenderInfo
         {
             public string Subject { get; set; }
@@ -26,54 +20,76 @@ namespace TenderProject
             public string ExpirationDate { get; set; }
             public string Law { get; set; }
             public string Link { get; set; }
+
+            public TenderInfo(string[] rawData)
+            {
+                Subject = rawData[0];
+                Customer = rawData[1];
+                ExpirationDate = rawData[2];
+                Law = rawData[3];
+                Link = rawData[4];
+            }
         }
+
+        private List<TenderInfo> tenderItems = new List<TenderInfo>(); // Создаем список для хранения объектов TenderInfo
+
         public MainWindow()
         {
             InitializeComponent();
             Loaded += MainWindow_Loaded;
         }
 
+        public class FileHelper
+        {
+            public static string[] GetFilesInDirectoryWithExtension(string directoryPath, string extension)
+            {
+                if (!Directory.Exists(directoryPath))
+                {
+                    Console.WriteLine($"Директория {directoryPath} не существует.");
+                    return new string[0];
+                }
+
+                try
+                {
+                    string[] files = Directory.GetFiles(directoryPath, $"*.{extension}");
+                    return files;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка при поиске файлов: {ex.Message}");
+                    return new string[0];
+                }
+            }
+        }
+
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            TenderInfo[] tenderItems = new[]
-           {
-                new TenderInfo
-                {
-                    Subject = "бинты",
-                    Customer = "Мэрия Мухсранска",
-                    ExpirationDate = "10/09/2023",
-                    Law = "44ФЗ",
-                    Link = "https:/zakupki"
-                },
-                new TenderInfo
-                {
-                    Subject = "гондоны",
-                    Customer = "Мэрия УстьПиздюйск",
-                    ExpirationDate = "11/09/2023",
-                    Law = "223ФЗ",
-                    Link = "https:/zakupki"
-                }
-            };
 
-            TenderList.ItemsSource = tenderItems;
+            string[] filePaths = FileHelper.GetFilesInDirectoryWithExtension(DirectoryPath, Extension);
+
+            foreach (string filePath in filePaths)
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                TenderInfo tender = new TenderInfo(lines);
+                tenderItems.Add(tender); 
+            }
+
+            TenderList.ItemsSource = tenderItems; 
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             PassportTender passportTender = new PassportTender();
             passportTender.Show();
-
         }
 
         private void TenderList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             TenderInfo selectedTender = (TenderInfo)TenderList.SelectedItem;
-            
-                PassportTender passportTender = new PassportTender();
-                passportTender.InitializeTenderInfo(selectedTender);
-                passportTender.Show();
-            
-        }
 
+            PassportTender passportTender = new PassportTender();
+            passportTender.InitializeTenderInfo(selectedTender);
+            passportTender.Show();
+        }
     }
 }
