c6b2f6b683cb0c9b4b153d52ec0025e9fa39b9ff
mihan2007 <mihan2007@yandex.ru>
2024-04-20 17:55:54 +0300

    revert to 11 Aprill second

--- Model/BuisnessDomain/Customer.cs
+++ Model/BuisnessDomain/Customer.cs
@@ -1,4 +1,7 @@
-﻿namespace TenderProject.Model.BuisnessDomain
+﻿using System;
+using NotImplementedException = System.NotImplementedException;
+
+namespace TenderProject.Model.BuisnessDomain
 {
     public class Customer
     {
@@ -9,5 +12,35 @@
         public string OGRN { get; set; }
         public string CustomerContactInfo { get; set; }
 
+        public Customer Copy()
+        {
+            var copy = new Customer();
+            copy.Name = Name;
+            copy.PostAdress = PostAdress;
+            copy.INN = INN;
+            copy.KPP = KPP;
+            copy.OGRN = OGRN;
+            copy.CustomerContactInfo = CustomerContactInfo;
+            return copy;
+        }
+
+        protected bool Equals(Customer other)
+        {
+            return Name == other.Name && PostAdress == other.PostAdress && INN == other.INN && KPP == other.KPP &&
+                   OGRN == other.OGRN && CustomerContactInfo == other.CustomerContactInfo;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Customer)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, PostAdress, INN, KPP, OGRN, CustomerContactInfo);
+        }
     }
 }
