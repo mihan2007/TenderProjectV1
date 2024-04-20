8eb947d873ec07358112e95952ca2e248bb799b6
mihan2007 <mihan2007@yandex.ru>
2024-04-20 18:07:48 +0300

    revert commit to 31 March

--- Model/BuisnessDomain/TenderInfo.cs
+++ Model/BuisnessDomain/TenderInfo.cs
@@ -1,6 +1,4 @@
 ﻿
-using System;
-
 namespace TenderProject.Model.BuisnessDomain
 {
     public class TenderInfo
@@ -10,44 +8,5 @@ namespace TenderProject.Model.BuisnessDomain
         public string TenderStatus { get; set; }
         public ProcedureInfo ProcedureInfo { get; set; }
         public Customer Customer { get; set; }
-
-        public TenderInfo Copy()
-        {
-            var copy = new TenderInfo();
-            copy.Id = Id;
-            copy.FilePath = FilePath;
-            copy.TenderStatus = TenderStatus;
-            copy.ProcedureInfo = ProcedureInfo.Copy();
-            copy.Customer = Customer.Copy();
-            return copy;
-        }
-
-        public void Reinit(TenderInfo source)
-        {
-            Id = source.Id;
-            FilePath = source.FilePath;
-            TenderStatus = source.TenderStatus;
-            ProcedureInfo = source.ProcedureInfo;
-            Customer = source.Customer;
-        }
-
-        protected bool Equals(TenderInfo other)
-        {
-            return Id == other.Id && FilePath == other.FilePath && TenderStatus == other.TenderStatus &&
-                   Equals(ProcedureInfo, other.ProcedureInfo) && Equals(Customer, other.Customer);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((TenderInfo)obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id, FilePath, TenderStatus, ProcedureInfo, Customer);
-        }
     }
 }
--- obj/Debug/netcoreapp3.1/TenderProject.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/TenderProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TenderProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb148cfe1d265827198ecbcb79c59f5b04ea8d18")]
 [assembly: System.Reflection.AssemblyProductAttribute("TenderProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TenderProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
